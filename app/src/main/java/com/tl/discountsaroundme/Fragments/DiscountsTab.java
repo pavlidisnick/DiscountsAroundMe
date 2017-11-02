e04a37594b40658600846dce8b9a07a6259c6ce9
Stefos <stefos96@hotmail.com>
2017-11-02 19:17:53 +0200

    Created class AddCategoryToLayout #3

--- app/src/main/java/com/tl/discountsaroundme/Fragments/DiscountsTab.java
+++ app/src/main/java/com/tl/discountsaroundme/Fragments/DiscountsTab.java
@@ -4,15 +4,20 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.tl.discountsaroundme.AddCategoryToLayout;
 import com.tl.discountsaroundme.Entities.Item;
 import com.tl.discountsaroundme.UiControllers.ItemSpaceDecoration;
 import com.tl.discountsaroundme.UiControllers.ItemViewAdapter;
@@ -31,6 +36,10 @@ public class DiscountsTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.grid_layout, container, false);
+
+        LinearLayout linearLayout = rootView.findViewById(R.id.linear_layout);
+//        Add Category with addCategoryToLayout.addCategory()
+        AddCategoryToLayout addCategoryToLayout = new AddCategoryToLayout(linearLayout, getActivity());
 
         mRecyclerView = rootView.findViewById(R.id.item_grid);
         mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));

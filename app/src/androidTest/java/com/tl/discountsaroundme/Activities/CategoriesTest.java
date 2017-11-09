c3d2fa0ae8d46ce9e9d49c1a079eb9673c7bf220
Stefos <stefos96@hotmail.com>
2017-11-09 20:47:08 +0200

    CategoriesTest created closes #44

---
+++ app/src/androidTest/java/com/tl/discountsaroundme/Activities/CategoriesTest.java
@@ -0,0 +1,125 @@
+package com.tl.discountsaroundme.Activities;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.tl.discountsaroundme.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class CategoriesTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void categoriesTest() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.clothes_tag), withText("Clothing"),
+                        childAtPosition(
+                                allOf(withId(R.id.linear_layout),
+                                        childAtPosition(
+                                                withId(R.id.horizontalScroll),
+                                                0)),
+                                1)));
+        appCompatButton.perform(scrollTo(), click());
+
+        ViewInteraction linearLayout = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.item_grid),
+                                childAtPosition(
+                                        withId(R.id.quickreturn_coordinator),
+                                        0)),
+                        0),
+                        isDisplayed()));
+        linearLayout.check(matches(isDisplayed()));
+
+        ViewInteraction linearLayout2 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.item_grid),
+                                childAtPosition(
+                                        withId(R.id.quickreturn_coordinator),
+                                        0)),
+                        1),
+                        isDisplayed()));
+        linearLayout2.check(matches(isDisplayed()));
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.clothes_tag), withText("Shoes"),
+                        childAtPosition(
+                                allOf(withId(R.id.linear_layout),
+                                        childAtPosition(
+                                                withId(R.id.horizontalScroll),
+                                                0)),
+                                7)));
+        appCompatButton2.perform(scrollTo(), click());
+
+        ViewInteraction linearLayout3 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.item_grid),
+                                childAtPosition(
+                                        withId(R.id.quickreturn_coordinator),
+                                        0)),
+                        0),
+                        isDisplayed()));
+        linearLayout3.check(matches(isDisplayed()));
+
+        ViewInteraction linearLayout4 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.item_grid),
+                                childAtPosition(
+                                        withId(R.id.quickreturn_coordinator),
+                                        0)),
+                        1),
+                        isDisplayed()));
+        linearLayout4.check(matches(isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}

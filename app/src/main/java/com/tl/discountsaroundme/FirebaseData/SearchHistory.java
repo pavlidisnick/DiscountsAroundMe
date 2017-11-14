699381319b02262e7afb14b258cf0c4b4259a063
Druit <nikolakis95@hotmail.com>
2017-11-14 16:37:12 +0200

    #7 make class for data (get,set) & firebase

--- app/src/main/java/com/tl/discountsaroundme/FirebaseData/SearchHistory.java
+++ app/src/main/java/com/tl/discountsaroundme/FirebaseData/SearchHistory.java
@@ -1,18 +1,38 @@
 package com.tl.discountsaroundme.FirebaseData;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.tl.discountsaroundme.Entities.History;
+
+import java.util.ArrayList;
 
 /**
  * Created by Druit on 11/11/2017.
  */
 
 public class SearchHistory {
-    private DatabaseReference search_history = FirebaseDatabase.getInstance().getReference("/user_references");
-
+    private DatabaseReference mDatasearch_history = FirebaseDatabase.getInstance().getReference("/user_references");
+    private ArrayList<String> Searches = new ArrayList<String>();
     private void NewSearchAdd(String search) {
 
-//in Progress
-        search_history.child("search_history").child("history").setValue("test");
+
+
+        mDatasearch_history.child("search_history").setValue("test");
+        mDatasearch_history.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    History Historyname = child.getValue(History.class);
+                    //To Be Continue
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }

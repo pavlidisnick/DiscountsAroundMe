f26a03586594a871de2d1d7a3729812e18ad4ebb
Druit33 <nikolakis95@hotmail.com>
2017-11-17 00:50:09 +0200

    #21 -  Fixed Type and discount text

--- app/src/main/java/com/tl/discountsaroundme/ItemDetailsActivity.java
+++ app/src/main/java/com/tl/discountsaroundme/ItemDetailsActivity.java
@@ -10,13 +10,15 @@ import com.tl.discountsaroundme.UiControllers.ItemViewAdapter;
 
 public class ItemDetailsActivity extends Activity {
 
-    String dataprice,dataitemName,dataitemDetails,datastoreName,dataImg;
+    String dataprice,dataitemName,dataitemDetails,datastoreName,dataImg,dataType,dataDiscount;
 
     TextView Price;
     TextView ItemName;
     TextView ItemDetails;
     TextView StoreName;
     ImageView imageView;
+    TextView Type;
+    TextView Discount;
 
 
 
@@ -34,20 +36,28 @@ public class ItemDetailsActivity extends Activity {
         datastoreName = intent.getStringExtra(ItemViewAdapter.DATA_ITEM_STORE);
         dataitemDetails = intent.getStringExtra(ItemViewAdapter.DATA_ITEM_DETAILS);
         dataImg = intent.getStringExtra(ItemViewAdapter.DATA_IMAGE);
+        dataType = intent.getStringExtra(ItemViewAdapter.DATA_TYPE);
+        dataDiscount = intent.getStringExtra(ItemViewAdapter.DATA_DISCOUNT);
 
 
+       // String correctPrice = DiscountFix(dataDiscount,dataprice);
 
-
-        Price =findViewById(R.id.price);
+        Price = findViewById(R.id.price);
         ItemDetails = findViewById(R.id.description);
         ItemName = findViewById(R.id.item);
         StoreName = findViewById(R.id.store);
         imageView = findViewById(R.id.img);
+        Type = findViewById(R.id.type);
+        Discount = findViewById(R.id.correctPrice);
 
-        Price.setText(dataprice);
+
+        Price.setText(" "+dataprice);
         ItemDetails.append(dataitemDetails);
         ItemName.setText(dataitemName);
         StoreName.append(datastoreName);
+        Type.append(dataType);
+        Discount.setText("30%");
+
 
 //        byte[] decodeStringImg = Base64.decode(dataImg,Base64.DEFAULT);
 //        Bitmap decoded = BitmapFactory.decodeByteArray(decodeStringImg,0, decodeStringImg.length);
@@ -55,5 +65,15 @@ public class ItemDetailsActivity extends Activity {
 
 
 
+    }
+
+    private String DiscountFix(String dataDiscount,String dataprice) {
+        String discount;
+        dataDiscount = "0."+dataDiscount;
+        float price = Float.parseFloat(dataprice);
+        float disc = Float.parseFloat(dataDiscount);
+        float afterDiscount = price-(price*disc);
+        discount = Float.toString(afterDiscount);
+        return discount;
     }
 }
--- app/src/main/java/com/tl/discountsaroundme/UiControllers/ItemViewAdapter.java
+++ app/src/main/java/com/tl/discountsaroundme/UiControllers/ItemViewAdapter.java
@@ -1,5 +1,6 @@
 package com.tl.discountsaroundme.UiControllers;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.tl.discountsaroundme.Activities.MainActivity;
 import com.tl.discountsaroundme.Entities.Item;
 import com.tl.discountsaroundme.ItemDetailsActivity;
 import com.tl.discountsaroundme.R;
@@ -25,8 +27,12 @@ public class ItemViewAdapter extends RecyclerView.Adapter<ItemViewAdapter.ItemVi
     public final static String DATA_ITEM_PRICE = "PRICE";
     public final static String DATA_ITEM_STORE = "STORE";
     public final static String DATA_IMAGE = "BitmapImage";
+    public final static String DATA_TYPE = "TYPE";
+    public final static String DATA_DISCOUNT = "DISCOUNT";
     private Context context;
     private ArrayList<Item> items;
+    private String itemType;
+    private double discount;
 
 
     public ItemViewAdapter(Context context, ArrayList<Item> items) {
@@ -52,6 +58,9 @@ public class ItemViewAdapter extends RecyclerView.Adapter<ItemViewAdapter.ItemVi
         holder.tvItemName.setText(items.get(position).getName());
         holder.tvItemDetails.setText(items.get(position).getDescription());
         holder.tvStoreName.setText(items.get(position).getStore());
+       itemType = items.get(position).getType().toString();
+       discount = ((float) items.get(position).getDiscount());
+
         // holder.tvStoreName.setText(storeList[position]);
         holder.tvPrice.setText(Double.toString(items.get(position).getPrice()) + " $");
         RequestOptions options = new RequestOptions();
@@ -84,7 +93,7 @@ public class ItemViewAdapter extends RecyclerView.Adapter<ItemViewAdapter.ItemVi
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Toast.makeText(context,tvItemName.getText(), Toast.LENGTH_LONG).show();
+                   // Toast.makeText(context,tvItemName.getText(), Toast.LENGTH_LONG).show();
 
                     //ItemDetails((ImageView) itemView,tvItemName,tvStoreName,tvPrice,tvItemDetails);
                     String details = tvItemDetails.getText().toString();
@@ -92,16 +101,16 @@ public class ItemViewAdapter extends RecyclerView.Adapter<ItemViewAdapter.ItemVi
                     String price = tvPrice.getText().toString();
                     String storeName = tvStoreName.getText().toString();
                     String img = imageView.getDrawable().toString();
+                    String itemDiscount = Double.toString(discount);
 
-
-                    ItemDetails(details,itemName,price,storeName,img);
+                    ItemDetails(details,itemName,price,storeName,img,itemType,itemDiscount);
                 }
             });
 
         }
 
     }
-    public void ItemDetails(String dataItemDetails, String dataItemName, String dataPrice, String dataStoreName, String img){
+    public void ItemDetails(String dataItemDetails, String dataItemName, String dataPrice, String dataStoreName, String img,String dataType,String dataDiscount){
 
         Intent ItemDetailsActivit=new Intent(context, ItemDetailsActivity.class);
 
@@ -110,7 +119,8 @@ public class ItemViewAdapter extends RecyclerView.Adapter<ItemViewAdapter.ItemVi
         ItemDetailsActivit.putExtra(DATA_ITEM_STORE,dataStoreName);
         ItemDetailsActivit.putExtra(DATA_ITEM_PRICE,dataPrice);
         ItemDetailsActivit.putExtra(DATA_IMAGE,img);
-
+        ItemDetailsActivit.putExtra(DATA_TYPE,dataType);
+        ItemDetailsActivit.putExtra(DATA_DISCOUNT,dataDiscount);
         context.startActivity(ItemDetailsActivit);
     }
 }

602dd03b1006e96e6736397303978e12a0fd53c4
pavlidisnick <pavlidis.nik95@gmail.com>
2017-11-19 00:06:25 +0200

    #16-NotificationReceiverActivity & result.xml need fix

--- app/src/main/java/com/tl/discountsaroundme/Services/Notifications/NotificationReceiverActivity.java
+++ app/src/main/java/com/tl/discountsaroundme/Services/Notifications/NotificationReceiverActivity.java
@@ -4,13 +4,35 @@ package com.tl.discountsaroundme.Services.Notifications;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.widget.TextView;
+import com.tl.discountsaroundme.FirebaseData.StoreManager;
 import com.tl.discountsaroundme.R;
+import com.tl.discountsaroundme.Services.GPSTracker;
 
 public class NotificationReceiverActivity extends Activity {
+
+    private StoreManager storeManager = new StoreManager();
+    private GPSTracker gps;
+    private double distance = 1; // in km
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.result);
+        /*setContentView(R.layout.result);*/
+        TextView tvNearbyStores = findViewById(R.id.textViewNearbyStores);
+        tvNearbyStores.setText((CharSequence) storeManager.getNearbyStores(gps.getLatitude(),gps.getLongitude(),distance*1000));
+
+
+
+
+
+
     }
+
+
 }

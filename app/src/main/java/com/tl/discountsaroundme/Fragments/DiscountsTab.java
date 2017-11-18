aaa6241d43348e427a7036b73917b8d793bdf530
Stefos <stefos96@hotmail.com>
2017-11-18 06:03:26 +0200

    Voice Search for discounted items #60

--- app/src/main/java/com/tl/discountsaroundme/Fragments/DiscountsTab.java
+++ app/src/main/java/com/tl/discountsaroundme/Fragments/DiscountsTab.java
@@ -1,16 +1,18 @@
 package com.tl.discountsaroundme.Fragments;
 
-import android.annotation.SuppressLint;
-import android.content.SharedPreferences;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.speech.RecognizerIntent;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -20,25 +22,31 @@ import com.arlib.floatingsearchview.suggestions.SearchSuggestionsAdapter;
 import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
 import com.google.firebase.auth.FirebaseAuth;
 import com.tl.discountsaroundme.AddCategoryToLayout;
+import com.tl.discountsaroundme.CategoryListener;
 import com.tl.discountsaroundme.Discounts.SearchSuggest;
 import com.tl.discountsaroundme.Discounts.SuggestListMaker;
 import com.tl.discountsaroundme.FirebaseData.DiscountsManager;
 import com.tl.discountsaroundme.R;
 import com.tl.discountsaroundme.UiControllers.ItemSpaceDecoration;
 import com.tl.discountsaroundme.UiControllers.ItemViewAdapter;
-import com.tl.discountsaroundme.CategoryListener;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 public class DiscountsTab extends Fragment {
     public static int discountValue = 30;
     FloatingSearchView mSearchView;
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    public static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
+    DiscountsManager discountsManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.grid_layout, container, false);
 
-        final DiscountsManager discountsManager = new DiscountsManager();
+        discountsManager = new DiscountsManager();
 
         final RecyclerView mRecyclerView = rootView.findViewById(R.id.item_grid);
         mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
@@ -75,7 +83,7 @@ public class DiscountsTab extends Fragment {
                 searchSuggestList = suggestListMaker.convertStringsToSuggestions(discountsManager.getSuggestionsDiscounts(), newQuery);
 
                 //pass them on to the search view
-                 mSearchView.swapSuggestions(searchSuggestList);
+                mSearchView.swapSuggestions(searchSuggestList);
             }
         });
 
@@ -109,8 +117,31 @@ public class DiscountsTab extends Fragment {
             }
         });
 
+        mSearchView.setOnMenuItemClickListener(new FloatingSearchView.OnMenuItemClickListener() {
+            @Override
+            public void onActionMenuItemSelected(MenuItem item) {
+                startVoiceRecognitionActivity();
+            }
+        });
+
         return rootView;
     }
 
+    public void startVoiceRecognitionActivity() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Speech recognition demo");
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
+            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            discountsManager.getDiscountsByName(matches.get(0));
+            mSearchView.setSearchText(matches.get(0));
+        }
+    }
 }

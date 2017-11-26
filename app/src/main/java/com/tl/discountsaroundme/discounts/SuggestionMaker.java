2706d2b666a865c10d038e68b9de8a91e601047c
Stefos <stefos96@hotmail.com>
2017-11-26 22:50:00 +0200

    #69 Search class created and cleaned up some unnecessary code

---
+++ app/src/main/java/com/tl/discountsaroundme/discounts/SuggestionMaker.java
@@ -0,0 +1,32 @@
+package com.tl.discountsaroundme.discounts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SuggestionMaker {
+
+    /**
+     * @param searchItems Array of items you want to search
+     * @param searchQuery a String query to search from the list of items
+     * @return List of suggestions based on the query passed as an arg
+     */
+    public List<String> getSuggestionsWithQuery(List<String> searchItems, String searchQuery) {
+        List<String> suggestStrings = new ArrayList<>();
+
+        for (String searchItem : searchItems) {
+            if (searchItem.toUpperCase().contains(searchQuery.toUpperCase()))
+                suggestStrings.add(searchItem);
+        }
+
+        return suggestStrings;
+    }
+
+    public List<SearchSuggest> stringsToSuggestions(List<String> suggestionStrings) {
+        List<SearchSuggest> searchSuggestList = new ArrayList<>();
+
+        for (String suggestionString : suggestionStrings)
+            searchSuggestList.add(new SearchSuggest(suggestionString));
+
+        return searchSuggestList;
+    }
+}

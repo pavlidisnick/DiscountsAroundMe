dd5e6d549bfa2fd7f00c916add0c24dab10dfe95
Stefos <stefos96@hotmail.com>
2017-12-04 06:07:06 +0200

    closes #65

--- app/src/main/java/com/tl/discountsaroundme/services/GPSTracker.java
+++ app/src/main/java/com/tl/discountsaroundme/services/GPSTracker.java
@@ -7,7 +7,6 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
